0e4ea664c1cd194adb3114ae1cf47c25280e472f
srivenkat13 <srivenkatsairaj13@gmail.com>
2023-12-19 17:27:34 +0530

    Board Component

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,17 +1,22 @@
 import { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 
 const Board = () => {
   const canvasRef = useRef(null);
+  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+  const { color, size } = useSelector((state) => state.toolbar[activeMenuItem]);
 
   useEffect(() => {
-    if (!canvasRef.current) return
-    const canvas = canvasRef.current
-    const context = canvas.getContext("2d")
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
 
     //when compontent mounted
-    canvas.width = window.innerWidth 
-    canvas.height = window.innerHeight
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
   }, []);
+  console.log(activeMenuItem, color, size);
   return <canvas ref={canvasRef}></canvas>;
 };
 
